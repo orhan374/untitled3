9cde7b8a9e68aa6e24019ae2c5e5343fb99d2b78
orhan374 <orhndgnr@gmail.com>
2022-04-12 11:43:22 +0300

    calismalar

---
+++ src/çalışmalar.java
@@ -0,0 +1,23 @@
+
+
+    public class çalışmalar {
+        public static void main(String[] args) {
+//        int x=5;
+//        System.out.println(x);
+//        x++;
+//        System.out.println(x);
+//        ++x;
+//        System.out.println(x);
+//
+//        int z=x++ +5;
+//        System.out.println(z);
+//        System.out.println(x);
+
+            int x=5;
+            x=x+7;
+            System.out.println(x);
+            x=x-2;
+            System.out.println(x);
+        }
+    }
+
